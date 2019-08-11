964bf83d5b5ae9bae1d6a7622f92a5d8601bfce4
weimin yang <yangwm1983@gmail.com>
2019-08-11 01:42:00 -0700

    Updates

---
+++ precache-manifest.13418aaa891f9dcb9f76d10d3067ac21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3796cb6b460f454a68b53c8d095138e",
+    "url": "/react-frontend-heroku/index.html"
+  },
+  {
+    "revision": "9ed9baf5da734f4e1349",
+    "url": "/react-frontend-heroku/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9d417e0657cc8abb0dbd",
+    "url": "/react-frontend-heroku/static/js/2.ea480d3e.chunk.js"
+  },
+  {
+    "revision": "9ed9baf5da734f4e1349",
+    "url": "/react-frontend-heroku/static/js/main.8b84f787.chunk.js"
+  },
+  {
+    "revision": "207c16de5f8e5637b036",
+    "url": "/react-frontend-heroku/static/js/runtime~main.413b7d1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-frontend-heroku/static/media/logo.5d5d9eef.svg"
+  }
+]);
